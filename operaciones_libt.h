5fa8776958b9e5384513f643eeafff09aa94dc8b
Mariano, Meza Montiel <marianomeza.uni@gmail.com>
2025-09-17 21:13:15 -0300

    Modifiqué Localizar_LIBT (a consultar). Agregué resto de operaciones del libt

--- operaciones_libt.h
+++ operaciones_libt.h
@@ -23,15 +23,15 @@ void init_LIBT(LIBT *libt) {
 }
 
 int Localizar_LIBT(LIBT libt, char *codigo, int *pos, float *costo) {
-    //límites inclusivos y segmento más grande a la derecha
-    //a costo se le suma 1 por cada comparación de clave en la trisección en el arreglo de punteros
+    //lï¿½mites inclusivos y segmento mï¿½s grande a la derecha
+    //a costo se le suma 1 por cada comparaciï¿½n de clave en la trisecciï¿½n en el arreglo de punteros
 
     int li = 0, ls = libt.ultimo_p - 1;
     int tercio, medio1, medio2;
     *costo = 0;
 
     while (li <= ls) {
-        if (ls - li + 1 < 3) { // Si el segmento es muy chico, se usa búsqueda secuencial
+        if (ls - li + 1 < 3) { // Si el segmento es muy chico, se usa bï¿½squeda secuencial
             int i = li;
             while (i <= ls && stricmp(libt.punteros[i]->codigo, codigo) < 0) {
                 *costo = *costo + 1;
@@ -48,37 +48,37 @@ int Localizar_LIBT(LIBT libt, char *codigo, int *pos, float *costo) {
         medio1 = li + tercio;
         medio2 = ls - tercio;
 
-        *costo = *costo + 1; // Primera comparación (medio1)
+        *costo = *costo + 1; // Primera comparaciï¿½n (medio1)
         if (strcmp(codigo, libt.punteros[medio1]->codigo) >= 0) {
-            //el código está en el primer o seg tercio
-            ls = medio - 1;
+            //el cï¿½digo estï¿½ en el primer o seg tercio
+            ls = medio2 - 1;
             if (strcmp(codigo, libt.punteros[medio1]->codigo) == 0) {
                 *pos = medio1;
                 return 1;
             }
-        } else { //está en el tercer tercio
+        } else { //estï¿½ en el tercer tercio
             *costo = *costo + 1; // segunda comp (medio2)
             if (stricmp(codigo, libt.punteros[medio2]->codigo) <= 0) {
-                //el cód está en el seg tercio
+                //el cï¿½d estï¿½ en el seg tercio
                 li = medio1 + 1;
                 ls = medio2;
                  if (stricmp(codigo, libt.punteros[medio2]->codigo) == 0) {
                     *pos = medio2;
                     return 1;
                 }
-            } else { // está en el tercer tercio
+            } else { // estï¿½ en el tercer tercio
                 li = medio2 + 1;
             }
 
             *pos = li;
-            return 0; //no está
+            return 0; //no estï¿½
         }
     }
 }
 
 int Alta_LIBT(LIBT *libt, Alumno alumno, float *costo) {
     //Agrega el alumno, lo guarda en en arreglo de alumnos y luego inserta el puntero a ese dato en la pos correcta del arr punteros
-    //el csoto sería el de localizar + 0.5 (0.5 por cada corrimiento de puntero)¿?
+    //el csoto serï¿½a el de localizar + 0.5 (0.5 por cada corrimiento de puntero)ï¿½?
 
     int pos;
     *costo = 0;
@@ -99,9 +99,9 @@ int Evocacion_LIBT(LIBT libt, char *cod, float *costo) {
 }
 
 int Baja_LIBT(LIBT *libt, Alumno alumno, float *costo) {
-    //Elimina al alumno. Busca el puntero en el arreglo punteros, lo elimina y desplaza los demás.
-    //El dato en el arreglo no se borraría pero sí quedaría inaccesible
-    //costo de localizar más 0.5 por cada corrimiento de puntero
+    //Elimina al alumno. Busca el puntero en el arreglo punteros, lo elimina y desplaza los demï¿½s.
+    //El dato en el arreglo no se borrarï¿½a pero sï¿½ quedarï¿½a inaccesible
+    //costo de localizar mï¿½s 0.5 por cada corrimiento de puntero
 }
 
 
